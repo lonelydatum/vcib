a0d97d5b2a190d8a6683636da2c2505076ea1d6d
Gar <gar@lonelydatum.com>
2021-02-26 16:16:41 -0500

    ON

--- dev/habitat_728x90/_js/main.js
+++ dev/habitat_728x90/_js/main.js
@@ -17,7 +17,7 @@ function start(){
 		// console.log(endTime-startTime);
 	}})
 
-	tl.set(".frameEnd", {x:w})
+	tl.set([".frameEnd"], {x:w})
 
 	tl.set(".frame2", {opacity:1})
 
@@ -27,13 +27,13 @@ function start(){
 		
 	tl.add("f2-in", "+=1")
 	tl.to(".t1", .4, {x:-185}, "f2-in")
-	tl.to(".big-red", .4, {x:-400}, "f2-in")
+	tl.to(".frame2 .big-red", .4, {x:-400}, "f2-in")
 	tl.from([".f2_logo", ".f2_plus"], .4, {opacity:0, x:"+=100"}, "f2-in")
 
 
 	tl.add("f3-in", "+=1")
 	tl.to([".f2_logo", ".f2_plus"], .4, {opacity:0, x:"-=100"}, "f3-in")
-	tl.to([".t1", ".big-red"], .4, {x:"-=500"}, "f3-in")
+	tl.to([".t1", ".frame2 .big-red"], .4, {x:"-=500"}, "f3-in")
 	tl.to(".photo", .4, {opacity:0}, "f3-in")
 	// tl.to(, .4, {x:"-=200"}, "f3-in")
 
@@ -42,34 +42,17 @@ function start(){
 	tl.add("f3a-in", "+=3.2")
 	tl.add(f3a_(), "f3a-in")
 
-
-	tl.add("f3b-in", "+=1")
-	tl.add(f3b_(), "f3b-in")
-
-	tl.add(end_(), "+=1")
-
 	// tl.gotoAndPlay("f3a-in")
 
 	
+	// tl.add("f3b-in", "+=1")
+	// tl.add(f3b_(), "f3b-in")
 
-	// return {tl, read:read[type]}
+	tl.add(end_(), "+=1")
 
+	
 
-	// const {tl, read} = init("habitat") 
 	
-	
-	
-	// tl.add(f2(), "f2-in")
-			
-	// tl.add("f3a-in", `+=${read}`)
-	
-	// tl.add(f3a(), "f3a-in")
-
-	// tl.add(f3b())
-	
-	
-	// tl.add("end-in", "+=.5")
-	// tl.add(end(), "end-in")
 	
 	
 
@@ -78,11 +61,19 @@ function start(){
 
 
 function f3a_(){
+	const x = 350
 	const tl = new TimelineMax()	
 	tl.set(".frame3a", {opacity:1})
-	tl.to(".frame2", TIME_FRAME_SLIDE, {x:wMinus}, 0) 
-	tl.from(".frame3a", TIME_FRAME_SLIDE, {x:size.w}, 0)
+	tl.set(".frame3a .big-red", {x:size.w})
+	tl.to(".frame2", .6, {x:-x}, 0) 
+	tl.to(".frame3a .big-red", .4, {x:size.w-x}, 0)
 	tl.add(stag(["f3a_1", "f3a_2"]))
+
+	tl.add("leave", "+=1")
+	tl.to(".frame3a .big-red", .3, {x:0}, "leave")
+	tl.to([".f3a_1", ".f3a_2"], .3, {opacity:0}, "leave")
+
+	tl.add(stag(["f3b_1", "f3b_2"]), "+=.1")
 
 	
 
@@ -109,7 +100,7 @@ function f3b_(){
 function end_(){
 	const tl = new TimelineMax()
 	tl.set(".frameEnd", {opacity:1})
-	tl.to(".frame3b", TIME_FRAME_SLIDE, {x:wMinus}, "end-in")	
+	tl.to(".frame3a", TIME_FRAME_SLIDE, {x:wMinus}, "end-in")	
 	tl.to(".frameEnd", TIME_FRAME_SLIDE, {x:-0}, "end-in")	
 
 	tl.from(".end_txt", .3, {opacity:0}, "+=.2")
